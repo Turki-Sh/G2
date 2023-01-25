797cc083b066790ef9f11108e899dd5577b09554
Turki-Sh <113216016+Turki-Sh@users.noreply.github.com>
2023-01-25 11:34:39 +0300

    Add files via upload

---
+++ Lab 8 task 2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 2 , y = 8 ;
+	int *p = &x,*q = &y;
+	
+	cout<<"\n\n The program wow!! \n"<<endl;
+	
+	
+	cout<<"\nThe address of x and the value of x: "<<&x<<" "<<x<<endl;
+	cout<<"\nThe value of p and the value of *p: "<<p<<" "<<*p<<endl;
+	
+	
+	cout<<"\n\n\nThe address of y and the value of y: "<<&y<<" "<<y<<endl;
+	cout<<"\nThe value of q and the value of *q: "<<q<<" "<<*q<<endl;
+	
+	
+	
+	
+}
+
